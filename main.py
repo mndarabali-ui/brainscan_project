96a04c7aaa618e54d9beebc9ade2030335483d6b
Markus Ndara Bali <mndarabali@gmail.com>
2026-08-04 18:06:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-
-app = FastAPI()
-
-# serve folder src/static sebagai /static
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# root "/" tampilkan UI
-@app.get("/")
-def home():
-    return FileResponse("static/index.html")
+from src.app import app
